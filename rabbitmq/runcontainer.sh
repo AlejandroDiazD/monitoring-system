b43bbdd07686085037f73412e5d90d160a4bd161
AlejandroDiazD <adidi.diaz@gmail.com>
2025-01-29 19:47:43 +0100

    Fix runcontainer for rabbit broker

--- rabbitmq/runcontainer.sh
+++ rabbitmq/runcontainer.sh
@@ -1,8 +1,12 @@
 docker run -d --name rabbitmq-mqtt \
-  -p 15672:15672 \  # Management console port
-  -p 1883:1883 \    # MQTT port
-  -p 5672:5672 \    # AMQP port
+  -p 15672:15672 \
+  -p 1883:1883 \
+  -p 5672:5672 \
   rabbitmq:management
+
+# -p 15672:15672 \  # Management console port
+# -p 1883:1883 \    # MQTT port
+# -p 5672:5672 \    # AMQP port
 
 ##############################################
 # Follow these commands to enable mqtt plugin:
---
+++ todo.md
@@ -0,0 +1,13 @@
+Sensors
+- [x] Sensor simulator script printing values in logs
+- [] Sensor simulator publishing values in mqtt topics
+- [] Sensor simulator using simulated I2C input
+
+RabbitMQ
+- [] Runcontainer v1 script for running rabbit broker
+- [] 
+
+FastAPI
+
+
+Grafana
